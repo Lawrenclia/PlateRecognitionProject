988cbd93be9e07689acab326e56ddc4aab020432
Lawrenclia <chianghx@sjtu.edu.cn>
2025-11-10 11:10:50 +0800

    重构了ccpd)prosess代码，file_root需要按实际情况自行调整

--- ccpd_process.py
+++ ccpd_process.py
@@ -2,6 +2,8 @@ import os
 import shutil
 import cv2
 import numpy as np
+from tqdm import tqdm  
+
 def allFilePath(rootPath,allFIleList):
     fileList = os.listdir(rootPath)
     for temp in fileList:
@@ -12,55 +14,20 @@ def allFilePath(rootPath,allFIleList):
             allFilePath(os.path.join(rootPath,temp),allFIleList)
 
 def order_points(pts):
-    # initialzie a list of coordinates that will be ordered
-    # such that the first entry in the list is the top-left,
-    # the second entry is the top-right, the third is the
-    # bottom-right, and the fourth is the bottom-left
     pts=pts[:4,:]
     rect = np.zeros((5, 2), dtype = "float32")
  
-    # the top-left point will have the smallest sum, whereas
-    # the bottom-right point will have the largest sum
     s = pts.sum(axis = 1)
     rect[0] = pts[np.argmin(s)]
     rect[2] = pts[np.argmax(s)]
  
-    # now, compute the difference between the points, the
-    # top-right point will have the smallest difference,
-    # whereas the bottom-left will have the largest difference
     diff = np.diff(pts, axis = 1)
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
  
-    # return the ordered coordinates
     return rect
 
-def get_partical_ccpd():
-    ccpd_dir = r"/mnt/Gpan/BaiduNetdiskDownload/CCPD1/CCPD2020/ccpd_green"
-    save_Path = r"ccpd/green_plate"
-    folder_list = os.listdir(ccpd_dir)
-    for folder_name in folder_list:
-        count=0
-        folder_path = os.path.join(ccpd_dir,folder_name)
-        if os.path.isfile(folder_path):
-            continue
-        if folder_name == "ccpd_fn":
-            continue
-        name_list = os.listdir(folder_path)
-        
-        save_folder=save_Path
-        if not os.path.exists(save_folder):
-            os.mkdir(save_folder)
 
-        for name in name_list:
-            file_path = os.path.join(folder_path,name)
-            count+=1
-            if count>1000:
-                break
-            new_file_path =os.path.join(save_folder,name)
-            shutil.move(file_path,new_file_path)
-            print(count,new_file_path)
-        
 def get_rect_and_landmarks(img_path):
    file_name = img_path.split("/")[-1].split("-")
    landmarks_np =np.zeros((5,2))
@@ -131,66 +98,41 @@ def xywh2yolo(rect,landmarks_sort,img):
     # annotation[0, 13] = landmarks_sort[4][1] / h  # l4_y
     return annotation
 
-def yolo2x1y1x2y2(annotation,img):
-    h,w,c = img.shape
-    rect= annotation[:,0:4].squeeze().tolist()
-    landmarks=annotation[:,4:].squeeze().tolist()
-    rect_w = w*rect[2]
-    rect_h =h*rect[3]
-    rect_x =int(rect[0]*w-rect_w/2)
-    rect_y = int(rect[1]*h-rect_h/2)
-    new_rect=[rect_x,rect_y,rect_x+rect_w,rect_y+rect_h]
-    for i in range(5):
-        landmarks[2*i]=landmarks[2*i]*w
-        landmarks[2*i+1]=landmarks[2*i+1]*h
-    return new_rect,landmarks
-
 def write_lable(file_path):
     pass
 
 
 if __name__ == '__main__':
-   file_root = r"ccpd"
-   file_list=[]
-   count=0
-   allFilePath(file_root,file_list)
-   for img_path in file_list:
-        count+=1
-        # img_path = r"ccpd_yolo_test/02-90_85-173&466_452&541-452&553_176&556_178&463_454&460-0_0_6_26_15_26_32-68-53.jpg"
-        text_path= img_path.replace(".jpg",".txt")
-        img =cv2.imread(img_path)
-        rect,landmarks,landmarks_sort=get_rect_and_landmarks(img_path)
-        # annotation=x1x2y1y2_yolo(rect,landmarks,img)
-        annotation=xywh2yolo(rect,landmarks_sort,img)
-        str_label = "0 "
-        for i in range(len(annotation[0])):
-                str_label = str_label + " " + str(annotation[0][i])
-        str_label = str_label.replace('[', '').replace(']', '')
-        str_label = str_label.replace(',', '') + '\n'
-        with open(text_path,"w") as f:
-                f.write(str_label)
-        print(count,img_path)
-    # get_partical_ccpd()
-    # file_root = r"ccpd/green_plate"
-    # file_list=[]
-    # allFilePath(file_root,file_list)
-    # count=0
-    # for img_path in file_list:
-    #     img_name = img_path.split(os.sep)[-1]
-    #     if not "&" in img_name:
-    #         count+=1
-    #         os.remove(img_path)
-    #         print(count,img_path)
-
-        # new_rect,new_landmarks=yolo2x1y1x2y2(annotation,img)
-        # rect= [int(x) for x in  new_rect]
-        # cv2.rectangle(img,(rect[0],rect[1]),(rect[2],rect[3]),(255,0,0),2)
-        # colors=[(0,255,0),(0,255,255),(255,255,0),(255,255,255),(255,0,255)] #绿 黄 青 白 
-        # for i in range(5):
-        #   cv2.circle(img,(landmarks[2*i],landmarks[2*i+1]),2,colors[i],2)
-        # cv2.imwrite("1.jpg",img)
-    #    print(rect,landmarks)
-        # get_partical_ccpd()
+    file_root = r"D:/Files/openCV/dataset"
+    file_list=[]
+    allFilePath(file_root,file_list)
     
+    # 使用 tqdm(file_list...) 
+    for img_path in tqdm(file_list, desc="Processing Images"): 
+        
+        try: 
+            text_path= img_path.replace(".jpg",".txt")
+            img =cv2.imread(img_path)
+            
+            if img is None:
+                print(f"Warning: Could not read image {img_path}. Skipping.")
+                continue
+                
+            rect,landmarks,landmarks_sort=get_rect_and_landmarks(img_path)
+            annotation=xywh2yolo(rect,landmarks_sort,img)
+            str_label = "0 "
+            for i in range(len(annotation[0])):
+                    str_label = str_label + " " + str(annotation[0][i])
+            str_label = str_label.replace('[', '').replace(']', '')
+            str_label = str_label.replace(',', '') + '\n'
+            
+            with open(text_path,"w") as f:
+                    f.write(str_label)
+                    
+        except Exception as e: 
+            # 捕获所有可能的错误 (比如文件名解析失败)
+            print(f"\nError processing file {img_path}: {e}. Skipping.")
+        
+        
         
         
